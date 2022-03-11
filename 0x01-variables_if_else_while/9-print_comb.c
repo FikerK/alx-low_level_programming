ecd510a989a43db0b99511f003d4ecab94e092c7
FikerK <fikikass@gmail.com>
2022-03-11 15:17:44 -0800

    Print Comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print 0-9 separated by comma and space using purchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
