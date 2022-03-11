adadb66f9ed65b88c4b8c4167d38fcc7ba3401c4
FikerK <fikikass@gmail.com>
2022-03-11 14:21:19 -0800

    Print Tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - tnirp eht tebahpla ni esrever
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char az;
+
+	for (az = 'z'; az >= 'a'; az--)
+	{
+		putchar(az);
+	}
+	putchar('\n');
+	return (0);
+}
