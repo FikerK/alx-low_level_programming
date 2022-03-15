b107ba6915d5f434f1b44543c356b9a4e7d6b6ad
FikerK <fikikass@gmail.com>
2022-03-15 13:14:13 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,14 +13,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		a = 1;
+		_putchar('+');
 	}
 	else if (n == 0)
 	{
 		a = 0;
+		_putchar('0');
 	}
 	else if (n < 0)
 	{
 		a = -1;
+		_putchar('-');
 	}
 	return (a);
 }
