8b3b41cf73ffcf33f9755f1ccc49c8780cac7209
FikerK <fikikass@gmail.com>
2022-03-16 02:29:44 -0700

    Print Last Digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,11 +10,11 @@ int print_last_digit(int n)
 {
 	int a = 0;
 
-	if (n < 0)
-	{
-		n = -1 * n;
-	}
 	a = n % 10;
-	_putchar('0' + a);
+	if (a < 0)
+	{
+		a = -1 * a;
+	}
+	_putchar(a + '0');
 	return (a);
 }
