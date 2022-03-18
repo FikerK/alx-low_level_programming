7e4114c75008228eb441698392c74c2fd1cad347
FikerK <fikikass@gmail.com>
2022-03-17 18:38:59 -0700

    More Numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0-14 10 times
+ *
+ * comment each line later pls
+ *
+ * Return: null
+ */
+
+void more_numbers(void)
+{
+	int n, d, e;
+
+	for (n = 0; n <= 10; n++)
+	{
+		for (d = 0; d < 1; d++)
+		{
+			for (e = 0; e <= 10; e++)
+			{
+				if (e == 10)
+				{
+					d = 1;
+					e = 0;
+				}
+				if (d == 1)
+				{
+					_putchar(d + '0');
+				}
+				_putchar(e + '0');
+				if (d == 1 && e == 0)
+				{
+					e = 1;
+				}
+				if (d == 1 && e == 4)
+				{
+					e = 11;
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
