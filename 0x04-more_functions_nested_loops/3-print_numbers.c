a8eb278c285646f56ac95017448a6543ebf2eb5d
FikerK <fikikass@gmail.com>
2022-03-17 17:28:44 -0700

    Print Numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers 0-9
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return;
+}
