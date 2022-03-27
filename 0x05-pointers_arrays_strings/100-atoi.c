11be24b9fad93931d5098c26c0cc88ad2277d07a
FikerK <fikikass@gmail.com>
2022-03-27 13:19:19 -0700

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ * Return: int.
+ */
+
+int _atoi(char *s)
+{
+	/*n = number in string, sn = sign, a = digit of n, i = string number*/
+	int n = 0, sn = 1, a = 1, i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == 45)
+			sn *= -1;
+		if (*(s + i) >= 48 && *(s + i) <= 57)
+		{
+			n = (n * a) + (*(s + i) - '0');
+			a = 10;
+		}
+		i++;
+	}
+	if (sn == -1)
+		n *= sn;
+	return (n);
+}
---
+++ 0x05-pointers_arrays_strings/100-main1.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+
+int main(void)
+{
+	_atoi("98");
+	_atoi("-98");
+	return (0);
+}
