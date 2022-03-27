0680b18a25ef9f4fb61d0538c3387a027f7d9faf
FikerK <fikikass@gmail.com>
2022-03-27 08:09:55 -0700

    Fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - prints the first 98 Fibonacci numbers
  * Return: 0.
@@ -7,13 +8,13 @@
 int main(void)
 {
 	int i;
-	long fn, n;
+	unsigned long fn = 0, n = 1;
 
 	for (i = 0; i < 49; i++)
 	{
 		fn += n;
 		n += fn;
-		printf("%ld, %ld", fn, n);
+		printf("%lu, %lu", fn, n);
 		if (i < 48)
 			printf(", ");
 	}
