b18368d27a9330ed0023010e361bf9684e5a9d92
FikerK <fikikass@gmail.com>
2022-03-30 14:50:45 -0700

    Is Prime Number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "main.c"
+
+/**
+ * _sqrt_recursion -gives the natural square root of a number
+ * @n: number to be square rooted
+ * Return: sqrt of n.
+ */
+
+int _sqrt_recursion(int n)
+{
+
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * is_prime_number - distinguish if num is prime or not
+ * @n: number to be scrutinized
+ * Return: 1 if prime, 0 if not.
+ */
+
+int is_prime_number(int n)
+{
+	
+}
