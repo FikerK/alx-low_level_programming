a03eef15d8097367b7d0180f5ebb8defee423dfa
FikerK <fikikass@gmail.com>
2022-03-31 01:36:03 -0700

    Sqrt Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,33 @@
-#include "main.c"
+#include "main.h"
+
+/**
+ * _sqrt - gives square root
+ * @n: scrutinized integer
+ * @i: manupilater 1
+ * @j: manupilator 2
+ * Return: sqrt of n
+ */
+
+int _sqrt(int n, int i, int j)
+{
+	int a = 0;
+
+	if (n < 0)
+		n = -1 * n;
+	if (i == 0)
+	{
+		a = -1;
+		return (-1);
+	}
+	if (i * j == n)
+	{
+		a = i;
+		return (i);
+	}
+	else
+		_sqrt(n , i - 1, j - 1);
+	/*return (a);*/
+}
 
 /**
  * _sqrt_recursion -gives the natural square root of a number
@@ -8,5 +37,5 @@
 
 int _sqrt_recursion(int n)
 {
-
+	return (_sqrt(n, n, n));
 }
