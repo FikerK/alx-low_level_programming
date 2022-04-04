c386ce899a0c707c25e550f1ba5f9c550e58ac77
FikerK <fikikass@gmail.com>
2022-04-04 06:13:34 -0700

    Malloc Create Array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,6 +17,10 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (0);
 	ptr = malloc(size);
+	if (ptr == NULL)
+	{
+		return (0);
+	}
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
