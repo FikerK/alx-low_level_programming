2409d9bafcd8f4c41f8c75e0b1aa516ccd8162c4
FikerK <fikikass@gmail.com>
2022-04-04 07:34:16 -0700

    Malloc strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,12 +10,12 @@
 char *_strdup(char *str)
 {
 	char *ptr;
-	unsigned long i = 0;
+	unsigned long i = 0, size = sizeof(str) + 1;
 
-	ptr = malloc(sizeof(str) + 5);
-	if (str == NULL)
+	ptr = malloc(size);
+	if (str == NULL || ptr == NULL)
 		return (0);
-	for (i = 0; i <= sizeof(str); i++)
+	for (i = 0; i <= size; i++)
 	{
 		ptr[i] = str[i];
 	}
