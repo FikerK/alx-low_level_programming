ac97ef08be66eec8c00a7e06a88d8e848899681d
FikerK <fikikass@gmail.com>
2022-04-04 15:39:36 -0700

    Malloc strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,17 +14,14 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (0);
-	else
-	{
-		while (str[j])
-			j++;
-	}
+	while (str[j])
+		j++;
 	ptr = malloc(j + 1);
+	if (ptr == NULL)
+		return (0);
 	for (i = 0; i <= j; i++)
 	{
 		ptr[i] = str[i];
 	}
-	if (ptr == NULL)
-		return (0);
 	return (ptr);
 }
