422b5b24bbe3df6c1cb6e7a5c74e37d3e90c119c
FikerK <fikikass@gmail.com>
2022-04-11 20:21:04 -0700

    Structure New

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,30 +14,32 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *d;
 	int i, n = 0, o = 0;
 
-	if (name == NULL || owner == NULL)
+	if (name == NULL || age < 0 || owner == NULL)
 		return (0);
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
+	{
 		return (0);
+	}
 	while (name[n])
 		n++;
-	d->name = malloc(sizeof(char) * n);
+	d->name = malloc(sizeof(char) * (n + 1));
 	if (d->name == NULL)
 	{
 		free(d);
-		return (NULL);
+		return (0);
 	}
 	for (i = 0; i < n; i++)
 		d->name[i] = name[i];
 	d->age = age;
 	while (owner[o])
 		o++;
-	d->owner = malloc(sizeof(char) * o);
+	d->owner = malloc(sizeof(char) * (o + 1));
 	if (d->owner == NULL)
 	{
 		free(d->name);
 		free(d);
-		return (NULL);
+		return (0);
 	}
 	for (i = 0; i < o; i++)
 		d->owner[i] = owner[i];
