b86ec50d75566c5386eba60cbfb4ca1352ba967a
FikerK <fikikass@gmail.com>
2022-04-21 16:26:38 -0700

    Free List

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: pointer to the list
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *h;
+
+	while(head)
+	{
+		h = head;
+		head = head->next;
+		free(h->str);
+		free(h);
+	}
+}
