11b5179ed7695bed3274409c53e37e8bf2e49b41
FikerK <fikikass@gmail.com>
2022-04-26 12:27:12 -0700

    List Int Length

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,7 +2,8 @@
 #define _LISTS_H_
 
 #include <stddef.h>
-#include <stdlib.h>
+#include <stdlib.h>i
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -18,5 +19,6 @@ typedef struct listint_s
 
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 #endif /*_LISTS_H_*/
