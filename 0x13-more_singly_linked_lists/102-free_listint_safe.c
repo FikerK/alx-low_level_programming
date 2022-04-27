e81be6bc3ac2ab088f1781a99d85f7f833ff7ab2
FikerK <fikikass@gmail.com>
2022-04-26 20:17:05 -0700

    Free ListInt Safe

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a listint_t linked list
+ * @head: double pointer to the start of the list
+ * Return: the number of nodes in the list
+ */
+
+size_t free_listint_safe(listint_t **head)
+{}
